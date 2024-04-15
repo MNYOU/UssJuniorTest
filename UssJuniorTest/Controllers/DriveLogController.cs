eba093a200124e99dcd2b0d752aab63b40ceff97
Arman Mukanov <mukanovarman777@gmail.com>
2024-04-15 22:57:08 +0500

    feat: добавил эндпоинт получения отчетов

--- UssJuniorTest/Controllers/DriveLogController.cs
+++ UssJuniorTest/Controllers/DriveLogController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using UssJuniorTest.Application.Models.Request;
+using UssJuniorTest.Application.Services;
+using UssJuniorTest.Application.Sieve;
 
 namespace UssJuniorTest.Controllers;
 
+/// <summary>
+/// Контроллер для управления данными журнала поездок.
+/// </summary>
 [Route("api/driveLog")]
-public class DriveLogController : Controller
+public class DriveLogController : ControllerBase
 {
-    public DriveLogController()
+    private readonly IDriverReportsGenerator driverReportsGenerator;
+    
+    /// <summary>
+    /// Инициализирует новый экземпляр класса <see cref="DriveLogController"/>.
+    /// </summary>
+    /// <param name="driverReportsGenerator">Генератор отчетов о водителях.</param>
+    public DriveLogController(IDriverReportsGenerator driverReportsGenerator)
     {
+        this.driverReportsGenerator = driverReportsGenerator;
     }
 
-    // TODO
-    // public ??? GetDriveLogsAggregation(??? args)
-    // {
-    // return ???    
-    // }
+    /// <summary>
+    /// Получает агрегированные данные журнала поездок на основе указанных параметров.
+    /// </summary>
+    /// <param name="parameters">Параметры для фильтрации и агрегации журнала поездок.</param>
+    /// <returns>Агрегированные данные журнала поездок.</returns>
+    [HttpGet]
+    public IActionResult GetDriveLogsAggregation([FromQuery] Parameters parameters)
+    {
+        var res = driverReportsGenerator.CreateReport(parameters);
+
+        return Ok(res);
+    }
 }
